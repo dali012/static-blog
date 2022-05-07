2b3b1a93d64f5ea7899c3b0f1796b9e87125eb20
dali012 <dali.jerbi97@gmail.com>
2022-05-07 14:43:45 +0000

    fix: test user routing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
+import { Appbar } from '@/components';
 import { ColorModeProvider } from '@/providers/ColorMode';
 import { CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app';
+import Login from './login';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const user: boolean = false;
   return (
     <ColorModeProvider>
       <CssBaseline />
+      {user ? (
+        <>
+          <Appbar />
+          <Component {...pageProps} />
+        </>
+      ) : (
+        <Login />
+      )}
       <Component {...pageProps} />
     </ColorModeProvider>
   );
